87ad3e8af1caea28e82c17bd3bb65b8e5cfb261c
Anderson <eaglesquad86@gmail.com>
2020-01-07 09:49:22 +0800

    project-iin ehnii huwisagchidiig uusgesen

---
+++ app.js
@@ -0,0 +1,19 @@
+// тоглогчийн ээлжийг хадгалах хувьсагч
+// нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
+var activePlayer = 1;
+
+// тоглогчдийн цуглуулсан оноог хадгалах хувьсагч
+var scores = [0, 0];
+// тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+var roundScore = 0;
+// шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
+var dice = Math.floor(Math.random() * 6) + 1;
+// Програм эхлэхэд бэлтгэе
+document.querySelector("#score-0").textContent = 0;
+document.querySelector("#score-1").textContent = 0;
+document.querySelector("#current-0").textContent = 0;
+
+document.querySelector("#current-0").textContent = 0;
+document.querySelector("#current-1").textContent = 0;
+
+document.querySelector(".dice").style.display = "none";
