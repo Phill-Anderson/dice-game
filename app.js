d9fba3d7241361a9895286e082dbc0599c9bd3ad
Anderson <eaglesquad86@gmail.com>
2020-01-07 14:06:55 +0800

    New Gamme Button is running

--- app.js
+++ app.js
@@ -1,22 +1,37 @@
 // тоглогчийн ээлжийг хадгалах хувьсагч
-// нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
-var activePlayer = 0;
-
-// тоглогчдийн цуглуулсан оноог хадгалах хувьсагч
-var scores = [0, 0];
-// тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
-var roundScore = 0;
-// шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
-var diceNumber = Math.floor(Math.random() * 6) + 1;
-// Програм эхлэхэд бэлтгэе
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
+var activePlayer;
+// тоглогчдын глобаль оноог хадгалах хувьсагч
+var scores;
+// тоглогчын нэг ээлжинд авах оноог хадгалах хувьсагч
+var roundScore;
 
 var diceDom = document.querySelector(".dice");
-diceDom.style.display = "none"; // querySelector -оос илүү getElementBy функц нь хурдан ажиллана.
+initGame();
+function initGame() {
+  // нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
+  activePlayer = 0; // хэрэв энд var activePlayer = 0 ; гэж бичвэл activePlayer глобаль хувьсагчийн хуулбар нь болж санах ойд бичигдэх юм. өөрөөр хэлбэл глобалаар зарласан activePlayer ба энэ функц дотор var гэж зарласан activePlayer хувьсагчууд нь хоорондоо ялгаатай хувьсагчууд болох юм.
+  // тоглогчдийн цуглуулсан оноог хадгалах хувьсагч
+  scores = [0, 0];
+  // тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+  roundScore = 0;
+  // Програм эхлэхэд бэлтгэе
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  diceDom.style.display = "none"; // querySelector -оос илүү getElementBy функц нь хурдан ажиллана.
+  // Тоглогчдын нэрийг буцааж гаргах
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
 
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+
+  document.querySelector(".player-0-panel").classList.add("active");
+}
 document.querySelector(".btn-roll").addEventListener("click", function() {
   // энэ нь anonymous функц бөгөөд үүнийг програмд ганц удаа ганц газар дуудахад  ашигладаг. Anonymous функц нь нэргүй функц юм.
   var diceNumber = Math.floor(Math.random() * 6) + 1; // 1-6 - ийн хооронд санамсаргүй тоо авах хувьсагч
@@ -71,3 +86,5 @@ function switchToNextPlayer() {
   diceDom.style.display = "none";
   alert("Тоглогчийн ээлж солигдлоо");
 }
+
+document.querySelector(".btn-new").addEventListener("click", initGame);
