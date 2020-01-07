55f602e425539c2d66a4bc40b7aec7d5a99719be
Anderson <eaglesquad86@gmail.com>
2020-01-07 10:19:10 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -7,13 +7,19 @@ var scores = [0, 0];
 // тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
 var roundScore = 0;
 // шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө
-var dice = Math.floor(Math.random() * 6) + 1;
+var diceNumber = Math.floor(Math.random() * 6) + 1;
 // Програм эхлэхэд бэлтгэе
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
+document.getElementById("score-0").textContent = "0";
+document.getElementById("score-1").textContent = "0";
+document.getElementById("current-0").textContent = "0";
+document.getElementById("current-1").textContent = "0";
 
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
+var diceDom = document.querySelector(".dice");
+diceDom.style.display = "none"; // querySelector -оос илүү getElementBy функц нь хурдан ажиллана.
 
-document.querySelector(".dice").style.display = "none";
+document.querySelector(".btn-roll").addEventListener("click", function() {
+  // энэ нь anonymous функц бөгөөд үүнийг програмд ганц удаа ганц газар дуудахад  ашигладаг. Anonymous функц нь нэргүй функц юм.
+  var diceNumber = Math.floor(Math.random() * 6) + 1; // 1-6 - ийн хооронд санамсаргүй тоо авах хувьсагч
+  diceDom.style.display = "block";
+  diceDom.src = "dice-" + diceNumber + ".png";
+});
